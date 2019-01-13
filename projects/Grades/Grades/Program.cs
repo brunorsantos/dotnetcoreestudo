991ff7513ddf351d64f8f76cea144126c7c021fd
Bruno <bruno.rodrigo@animaeducacao.com.br>
2019-01-13 12:54:04 -0200

    novo projeto de notas

---
+++ projects/Grades/Grades/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(81);
+            book.AddGrade(91.5f); // incluir o f para considerar o numero como float e nao como double
+            book.AddGrade(100);
+
+    
+            GradeStatistics stats = book.ComputeStatistics();
+
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(stats.AverageGrade);
+        }
+    }
+}
--- projects/Hello/Hello/Program.cs
+++ projects/Hello/Hello/Program.cs
@@ -6,7 +6,20 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Quantas horas dormiu noite passada");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello World " + name);
+            if (hoursOfSleep > 8)
+            {
+                Console.WriteLine("Bem dormido");
+            } else
+            {
+                Console.WriteLine("Dorme mais");
+            }
+
         }
     }
 }
