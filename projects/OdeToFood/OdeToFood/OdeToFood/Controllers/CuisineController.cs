a5fbd5218fa805ec0b789ebfed61e3a96d00c552
Bruno <bruno.rodrigo@animaeducacao.com.br>
2019-02-20 12:06:03 -0300

    mvc4

---
+++ projects/OdeToFood/OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class CuisineController : Controller
+    {
+        // GET: Cuisine
+        public ActionResult Search(string name)
+        {
+
+            var ret = Server.HtmlEncode(name);
+            return Content(ret);
+        }
+    }
+}
---
+++ projects/OdeToFood/OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            var controller1 = RouteData.Values["controller"];
+
+            var model = new AboutModel();
+            model.Name = "teste";
+            model.Location = "Casa";
+            ViewBag.Message = controller1;
+            ViewBag.Location = "Casa";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
